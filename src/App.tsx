dd8856af8c58a0c7aca09141a8eb9c1884bf7eb0
Noisk8 <juansk8jaramillo@gmail.com>
2025-02-05 02:39:34 -0500

    wallet conectada, mostrando tokens

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 // import { useBlock } from "@starknet-react/core";
 import Header from "./components/Header";
+import Balances from "./components/starknet/Balances";
 
 function App() {
   return (
     <main className=" flex flex-col items-center justify-center min-h-screen gap-12">
       <Header />
+      <Balances />
      
       <div className="flex flex-row gap-12">
        
---
+++ src/components/starknet/Balances.tsx
@@ -0,0 +1,31 @@
+import { useAccount, useBalance } from "@starknet-react/core";
+
+export default function Balances() {
+  const { address } = useAccount();
+  const { isLoading, isError, error, data } = useBalance({
+    address,
+    
+    watch: true,
+  });
+
+  const  { isLoading: isBalanceLoading, isError: isBalanceError, error: balanceError, data: balanceData } = useBalance({
+
+    token: "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d",
+    address,
+    watch: true
+  })
+
+
+  if (isLoading || isBalanceLoading ) return <div>Loading...</div>;
+  if (isError || isBalanceError || !data) return <div>{error?.message || balanceError?.message}</div>;
+
+  return (
+    <div className="fixed">
+      {data.value.toString()} {data.symbol} 
+      <br/>
+      {balanceData?.formatted} {balanceData?.symbol}
+
+
+    </div>
+  );
+}
