5fa3a1710c49f1d2f40b149cb3bb0632fb33fa2a
Noisk8 <juansk8jaramillo@gmail.com>
2025-02-05 01:38:19 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import { useBlock } from "@starknet-react/core";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <main className=" flex flex-col items-center justify-center min-h-screen gap-12">
+      <Header />
+     
+      <div className="flex flex-row gap-12">
+       
+      
+      </div>
+    </main>
+  );
+}
+
+export default App;
